a26d982d45f65500873cf32acd1da1113d601612
ryan <ryan@2digit.io>
2024-10-25 18:46:59 +0900

    section02 진행중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-book";
 import fetchRandomBooks from "@/lib/fetch-randomBooks";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   console.log("index page");
@@ -25,20 +26,31 @@ export default function Home({
   recoBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((item) => (
-          <BookItem key={item.id} {...item} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((item) => (
-          <BookItem key={item.id} {...item} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((item) => (
+            <BookItem key={item.id} {...item} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((item) => (
+            <BookItem key={item.id} {...item} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetch-book";
+import Head from "next/head";
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
@@ -20,11 +21,22 @@ export default function Search({
   books,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div>
-      {books.map((item) => (
-        <BookItem key={item.id} {...item} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>한입북스-검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div>
+        {books.map((item) => (
+          <BookItem key={item.id} {...item} />
+        ))}
+      </div>
+    </>
   );
 }
 
